f58421c474cd43473da6058486dd95fff1d87b7f
mohamedIrfan3012 <42871545+mohamedIrfan3012@users.noreply.github.com>
2019-02-06 22:54:46 -0800

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,8 +1,8 @@
 list=['a','e','i','o','u','A','E','I','O','U']
 a=raw_input()
 if(a in list):
-	print('vowel')
+	print('Vowel')
 elif(a!=list):
-	print('consonant')
+	print('Consonant')
 else:
 	print('invalid')
