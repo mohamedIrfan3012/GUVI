ee852d45717ffded90edf2cb3b18e5136c60f260
mohamedIrfan3012 <42871545+mohamedIrfan3012@users.noreply.github.com>
2019-02-18 20:41:38 -0800

    Update code.py

--- code.py
+++ code.py
@@ -1,25 +1,30 @@
 source=0
 print("enter the destination")
-destination=int(input())
-if destination<1:
+destination=raw_input()
+if (destination.isalpha()):
+    print("invalid input")
+elif float(destination)<=0:
 	print("invalid destination")
-  
 else:
-  total=source+destination
+  total=source+float(destination)
   print("enter the choice")
   print("1.auto...2.mini...3.micro....4.premium")
-  ch=int(input())
-  if ch==1:
-        fare=15*total
-        print("the total fare =",fare)
-  elif ch==2:
+  ch=(raw_input())
+  if (ch.isalpha()) :
+		    print("invalid choice")
+        
+  elif float(ch)==2:
         fare=30*total
         print("the total fare =",fare)
-  elif ch==3:
+  elif float(ch)==3:
         fare =45*total
         print("the total fare =",fare)
-  elif ch==4:
+  elif float(ch)==4:
 		    fare=60*total
 		    print("the total fare =",fare)
-  else :
-		    print("invalid choice")
+  elif float(ch)==1:
+        fare=15*total
+        print("the total fare =",fare)
+        
+  else:
+          print("invalid choice")
